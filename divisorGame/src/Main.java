a5c36c1eecebffc036f064ef42033301b91d43c0
OzzkanBulut <ozkanb66@gmail.com>
2022-10-05 00:41:14 +0300

    Divisor Game

---
+++ divisorGame/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        DivisorGame divisorGame = new DivisorGame();
+        System.out.println(divisorGame.divisorGame(2));
+    }
+}
