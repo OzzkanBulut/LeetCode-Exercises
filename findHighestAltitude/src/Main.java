7494e6751b3dc8b8220aacfa9557ab455f357646
OzzkanBulut <ozkanb66@gmail.com>
2024-05-30 00:20:36 +0300

    Largest Altitude

---
+++ findHighestAltitude/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {44,32,-9,52,23,-50,50,33,-84,47,-14,84,36,-62,37,81,-36,-85,-39,67,-63,64,-47,95,91,-40,65,67,92,-28,97,100,81};
+        System.out.println(Solution.largestAltitude(arr));;
+    }
+}
---
+++ findHighestAltitude/src/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+
+    public static int largestAltitude(int[] gain) {
+//        Input: gain = [52,-91,72]
+//        Output: 0
+//        Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+        int sum = 0,alt = 0;
+        for(int i : gain){
+            if(alt <= sum){
+                alt = sum;
+                sum = sum +i;
+            }else{
+                sum = sum +i;
+            }
+        }
+        if(alt>sum){
+
+            return alt;
+        }else
+            return sum;
+    }
+}
